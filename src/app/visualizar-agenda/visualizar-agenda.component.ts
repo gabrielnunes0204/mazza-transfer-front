8f0252b089b97743234b83b06bd6457f52afca8a
Gabriel Nunes <gabriel.microsmp@gmail.com>
2025-02-22 11:53:29 -0300

    visualization component implementation

--- src/app/visualizar-agenda/visualizar-agenda.component.ts
+++ src/app/visualizar-agenda/visualizar-agenda.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
+import { RequestService } from '../service/request.service';
+import { TransferenciaModel } from '../model/transferencia.model';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
@@ -13,8 +16,29 @@ import { RouterModule } from '@angular/router';
 })
 export class VisualizarAgendaComponent implements OnInit {
 
-  constructor() { }
+  listaTransfer = [] as TransferenciaModel[];
+
+  constructor(private requestService: RequestService) { }
 
   ngOnInit(): void {
+    this.getTransfer();
+  }
+
+  getTransfer(): void {
+    this.requestService.getTransfers().subscribe({
+      next: (data: any) => {
+        console.log('Dados recebidos: ', data);
+        this.listaTransfer = data;
+      },
+      error: (error) => {
+        Swal.fire('Erro', 'Erro ao buscar as transferências.', 'error');
+        console.error('Erro ao buscar as transferências: ', error);
+      },
+      complete: () => { }
+    });
+  }
+
+  somatoriaTaxaTransferencia(valorTransferencia?: number, valorTaxa?: number): number {
+    return (valorTransferencia ?? 0) + (valorTaxa ?? 0);
   }
 }
