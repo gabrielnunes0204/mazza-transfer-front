85403ae45cba49be910edc10ec1256f5836e6e47
Gabriel Nunes <gabriel.microsmp@gmail.com>
2025-02-22 11:14:18 -0300

    add component visualizar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
+import { VisualizarAgendaComponent } from './visualizar-agenda/visualizar-agenda.component';
 import { HomeComponent } from './home/home.component';
 
 export const routes: Routes = [
-    { component: HomeComponent, path: "" }
+    { component: HomeComponent, path: "" },
+    { component: VisualizarAgendaComponent, path: "visualizar" }
 ];
